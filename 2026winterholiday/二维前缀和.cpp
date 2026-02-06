3f46c12d5bc11ee282e8f3a871c4916f8152b81d
Yuhan Chen <2061458608@qq.com>
2026-02-06 10:50:41 +0800

    更新 二维前缀和.cpp
    增加两种更快灵活方法

--- 2026winterholiday/二维前缀和.cpp
+++ 2026winterholiday/二维前缀和.cpp
@@ -35,3 +35,65 @@ int main(){
     
     return 0;
 }
+
+
+
+//原地计算
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(n+2, vector<int>(n+2, 0));
+    
+    for(int i = 0; i < m; i++){
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        a[x1][y1]++, a[x1][y2+1]--;
+        a[x2+1][y1]--, a[x2+1][y2+1]++;
+    }
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            a[i][j] += a[i-1][j] + a[i][j-1] - a[i-1][j-1];
+            cout << a[i][j] << (j == n ? "\n" : " ");
+        }
+    }
+    
+    return 0;
+}
+
+
+//原地计算加数组栈分配
+#include<iostream>
+using namespace std;
+
+int a[1002][1002];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    while(m--){
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        a[x1][y1]++, a[x1][y2+1]--;
+        a[x2+1][y1]--, a[x2+1][y2+1]++;
+    }
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            a[i][j] += a[i-1][j] + a[i][j-1] - a[i-1][j-1];
+            cout << a[i][j] << " \n"[j == n];
+        }
+    }
+    
+    return 0;
+}
