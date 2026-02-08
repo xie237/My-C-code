1dbadf0248e39f4e3c4335850d8c3c2e97d72d62
Yuhan Chen <2061458608@qq.com>
2026-02-08 18:13:56 +0800

    更新 a-b问题-哈希表统计.cpp
    适配c++11

--- 2026winterholiday/a-b问题-哈希表统计.cpp
+++ 2026winterholiday/a-b问题-哈希表统计.cpp
@@ -6,35 +6,36 @@ using namespace std;
 int main(){
     int n, c;
     cin >> n >> c;
-    
+
     vector<int> num(n);
     unordered_map<int, int> cnt;
-    
+
     for(int i = 0; i < n; i++){
         cin >> num[i];
         cnt[num[i]]++;
     }
-    
+
     long long ans = 0;  // 用long long，可能很大
-    
+
     if(c == 0){
-        // 特殊处理C=0
-        for(auto& [num, count] : cnt){
-            ans += (long long)count * (count - 1);
+        // 特殊处理C=0 - C++11兼容写法
+        for(unordered_map<int, int>::iterator it = cnt.begin(); it != cnt.end(); ++it){
+            long long count = it->second;  // 出现次数
+            ans += count * (count - 1);
         }
     } else {
-        // 正常情况
+        // 正常情况 - C++11兼容写法
         for(int i = 0; i < n; i++){
             int current = num[i];
             long long target = (long long)current + c;  // 防止溢出
-            
-            if(cnt.count(target)){
+
+            if(cnt.find(target) != cnt.end()){  // C++11风格查找
                 ans += cnt[target];
             }
         }
     }
-    
+
     cout << ans;
-    
+
     return 0;
 }
