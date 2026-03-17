12a3cdd8d08aad017ef51a02fbb5c9e9439a5630
Yuhan Chen <2061458608@qq.com>
2026-03-17 10:07:31 +0800

    创建 质因数分解-简单题.cpp

---
+++ 2026winterholiday/数学问题集/质因数分解-简单题.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin>>n;
+
+    for(int i=2;i<=n;i++){
+        if(n%i==0){
+            cout<<n/i;
+            break;
+        }
+    }
+    return 0;
+}
