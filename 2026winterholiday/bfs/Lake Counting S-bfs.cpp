92caae41183004fafe2c03877fed63039c62ebdb
Yuhan Chen <2061458608@qq.com>
2026-03-18 22:12:59 +0800

    更新 Lake Counting S-bfs.cpp

--- 2026winterholiday/bfs/Lake Counting S-bfs.cpp
+++ 2026winterholiday/bfs/Lake Counting S-bfs.cpp
@@ -56,3 +56,111 @@ int main(){
     cout<<ans;
     return 0;
 }
+
+
+
+//手敲第二次dfs和bfs
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+const int N = 105;
+const int M=105;
+char matrix[N][M];
+bool visited[N][M];
+int dx[8]={0,0,1,-1,1,-1,1,-1};
+int dy[8]={1,-1,0,0,1,-1,-1,1};
+int count=0;
+int n,m;
+
+void dfs(int x, int y){
+
+    visited[x][y]=true;
+    for(int i=0;i<8;i++){
+        int nx=x+dx[i];
+        int ny=y+dy[i];
+
+        if(nx>=0 && nx<n && ny>=0 && ny<m && matrix[nx][ny]=='W' && !visited[nx][ny]){
+            visited[nx][ny]=true;
+            dfs(nx, ny);
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>matrix[i][j];
+        }
+    }
+
+    memset(visited, false, sizeof(visited));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+                if(matrix[i][j]=='W' && !visited[i][j]) {dfs(i, j); count++;}
+        }
+    }
+    cout<<count;
+    return 0;
+}
+
+#include<iostream>
+#include<cstring>
+#include<queue>
+using namespace std;
+
+const int N=105;
+const int M=105;
+char matrix[N][M];
+bool visited[N][M];
+int dx[8]={0,0,1,-1,1,-1,1,-1};
+int dy[8]={1,-1,0,0,1,-1,-1,1};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n,m;
+    cin>>n>>m;
+    memset(visited, false, sizeof(visited));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>matrix[i][j];
+        }
+    }
+
+    int count=0;
+    queue<pair<int, int>>q;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(!visited[i][j] && matrix[i][j]=='W'){
+                q.push({i,j});
+                count++;
+            }
+
+            while(!q.empty()){
+                int x=q.front().first;
+                int y=q.front().second;
+                q.pop();
+
+                for(int k=0;k<8;k++){
+                    int nx=x+dx[k];
+                    int ny=y+dy[k];
+
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && matrix[nx][ny]=='W'){
+                        visited[nx][ny]=true;
+                        q.push({nx,ny});
+                    }
+                }
+            }
+        }
+    }
+
+    cout<<count;
+    return 0;
+}
