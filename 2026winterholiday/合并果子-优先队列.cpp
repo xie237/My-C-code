f78176261e79a2666fd417df0de7f7f375487c97
Yuhan Chen <2061458608@qq.com>
2026-03-29 11:25:02 +0800

    Create 合并果子-优先队列.cpp

---
+++ 2026winterholiday/合并果子-优先队列.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    priority_queue<int, vector<int>, greater<int>>heap;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        heap.push(a);
+    }
+
+    int ans=0;
+    while(heap.size()>1){
+        int a=heap.top(); heap.pop();
+        int b=heap.top(); heap.pop();
+        int sum = a+b;
+        ans += sum;
+        heap.push(sum);
+    }
+
+    cout<<ans;
+    return 0;
+}
+
+/*
+堆的操作	priority_queue的操作
+建堆	priority_queue<int> pq;
+插入元素	pq.push(x);
+取堆顶（最小/最大）	pq.top();
+删除堆顶	pq.pop();
+堆大小	pq.size();
+判空	pq.empty();
+*/
